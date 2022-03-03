5a66d09454fdea01a5ba912913bf1fd32d5dac00
Jacob Hite <1085644+jrhite@users.noreply.github.com>
2022-03-03 07:28:03 +0700

    remove localhost from hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,14 +5,5 @@ module.exports = {
   solidity: "0.7.5",
   paths: {
     artifacts: "./app/artifacts",
-  },
-  networks: {
-    localhost: {
-      url: "http://localhost:8545"
-    },
-    rinkeby: {
-      url: process.env.RINKEBY_URL,
-      accounts: [process.env.PRIVATE_KEY]
-    }
   }
 };
